0ccce8ffd788bdd70336353515dfa85635a2d19a
Ferooz Khan <feroozkhan.chintu@gmail.com>
2017-10-17 21:08:01 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,41 @@
+class Test {
+    public static void main(String args[]) {
+        for (int i = 0; i < 10;) {
+
+        }
+
+        for (int i = 0; i<10; i++) {
+
+        }
+
+        
+        String s;
+        for(;(s="22").length()>10;){
+            
+        }
+        
+        for(int a =0; a++<10;){
+            
+        }
+        
+        for(int i=1,j=0; i< 10;j++){
+            
+        }
+        
+        boolean b = false;
+
+        while (b){
+
+        }
+
+        while(b){
+            b = true;
+        }
+        
+        int a = 0;
+        
+        while(a++ > 1){
+            
+        }
+    }
+}
